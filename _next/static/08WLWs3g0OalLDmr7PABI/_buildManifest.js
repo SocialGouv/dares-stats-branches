9107083ca7b3b785a8154c069a7f3157e42930ca
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2022-07-20 18:37:59 +0200

    Updates

---
+++ _next/static/08WLWs3g0OalLDmr7PABI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/239c78f49fb1b7e2.css","static/chunks/pages/index-b2e199c710bda49d.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
--- _next/static/1TpycUcwlQlgi7hz9qSpu/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/239c78f49fb1b7e2.css","static/chunks/pages/index-574835941a629a4c.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
